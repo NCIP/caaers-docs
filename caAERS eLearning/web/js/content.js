1b435d430ae0b4eb3987b37ae7488089ed2ebfdc
Patrick McConnell <patrick.mcconnell@semanticbits.com>
2012-04-22 12:42:04 +0000

    SVN-Revision: 15655

--- caAERS eLearning/web/js/content.js
+++ caAERS eLearning/web/js/content.js
@@ -18,30 +18,42 @@ var roles = [
 		lessons: [
 		    jQuery.extend({}, caersIntro),
 			{ 
-		    	icon: "images/icons/create.png",
-				title: "Reporting Adverse Events",
+		    	icon: "images/icons/enter.png",
+				title: "Enter Adverse Events",
 				description: "Enter the details of adverse events and determine reporting requirements.",
 				swfUriLarge: "lessons/ae_coordinator/create/create.swf",
 				swfUriSmall: "lessons/ae_coordinator/create/create_640.swf"
 			},
 			{ 
-		    	icon: "images/icons/report.png",
-				title: "Expedited Report Creation",
+		    	icon: "images/icons/report_24_hour.png",
+				title: "24 Hour Report",
+				description: "Create and submit a 24 hour expedited adverse event report.",
+				swfUriLarge: "lessons/ae_coordinator/report_24_hour/report_24_hour.swf",
+				swfUriSmall: "lessons/ae_coordinator/report_24_hour/report_24_hour_640.swf"
+			},
+			{ 
+		    	icon: "images/icons/report_expedited.png",
+				title: "Expedited Report",
 				description: "Create and submit an expedited adverse event report.",
-				swfUriLarge: "lessons/ae_coordinator/report/report.swf",
-				swfUriSmall: "lessons/ae_coordinator/report/report_640.swf"
+				swfUriLarge: "lessons/ae_coordinator/report_expedited/report_expedited.swf",
+				swfUriSmall: "lessons/ae_coordinator/report_expedited/report_expedited_640.swf"
 			},
 			{ 
 		    	icon: "images/icons/edit.png",
-				title: "Report Editing",
+				title: "Edit Report",
 				description: "Edit an in-progress report.",
 				swfUriLarge: "lessons/ae_coordinator/edit/edit.swf",
 				swfUriSmall: "lessons/ae_coordinator/edit/edit_640.swf"
 			},
 			{ 
 		    	icon: "images/icons/amend.png",
-				title: "Report Amendment",
+				title: "Amend Report",
 				description: "Amend an already submitted report."
+			},
+			{ 
+		    	icon: "images/icons/export.png",
+				title: "Export Data",
+				description: "Export adverse events data for further research."
 			}
 		]
 		
@@ -52,6 +64,29 @@ var roles = [
 		headshot: "images/headshots/data_coordinator.png",
 		lessons: [
 		    jQuery.extend({}, caersIntro)
+		]
+		
+	},
+	{ title: "System Administrator",
+		shortTitle: "System Admin",
+		description: "Adds and edits users, updates system configuration.",
+		headshot: "images/headshots/sys_admin.png",
+		lessons: [
+		    jQuery.extend({}, caersIntro),
+			{ 
+		    	icon: "images/icons/add_user.png",
+				title: "Add Personnel",
+				description: "Add new personnel to caAERS, including people and users.",
+				swfUriLarge: "lessons/administrator/add_user/add_user.swf",
+				swfUriSmall: "lessons/administrator/add_user/add_user_640.swf"
+			},
+			{ 
+		    	icon: "images/icons/edit_user.png",
+				title: "Edit Existing Personnel",
+				description: "Edit existing personnel in caAERS, including modifying details and deactivating users.",
+				swfUriLarge: "lessons/administrator/edit_user/edit_user.swf",
+				swfUriSmall: "lessons/administrator/edit_user/edit_user_640.swf"
+			}
 		]
 		
 	}
